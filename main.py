437e8d3cb30cf3f22c152e9f6065c7fa072ca55a
feixuetuba <feixuetuba@126.com>
2020-06-16 08:31:18 +0800

    添加了灰度浮雕

--- main.py
+++ main.py
@@ -3,15 +3,20 @@ import cv2
 from utils.common import show, imopen
 from modules.distort import displacement
 from modules.mixture import overlay
-from modules.style import emboss
+from modules import style
 
 @show
 def show_effect():
+    import numpy as np
     checkerboard = imopen("resources/checkerboard.jpg")
     bg = imopen("resources/bg.png")
-    ret = displacement(bg, checkerboard, 10, 10)
-    ret = overlay(bg, ret)
-    return  ret
+    em1 = style.emboss(bg, offset=128)
+    em2 = style.emboss(bg, offset=128, height=2)
+    em3 = style.emboss(bg, offset=128, height=3)
+    em5 = style.emboss(bg, offset=128, height=5)
+    ret = displacement(em5, checkerboard, 30, 30)
+    ret = overlay(em5, ret)
+    return np.hstack([em1, em2, em3, em5, ret, bg])
 
 if __name__ == "__main__":
     show_effect()
--- modules/style.py
+++ modules/style.py
@@ -1,17 +1,30 @@
 """
 PS 风格
 """
+import math
+
 from cv2 import filter2D
 import numpy as np
 
-def emboss(image, gray_mode=False):
+ANGLE0 = (1,0)
+ANGLE45 = (0,0)
+ANGLE90 = (0,1)
+ANGLE135 = (0,2)
+ANGLE180 = (1,2)
+ANGLE225 = (2,2)
+ANGLE270 = (2,1)
+ANGLE315 = (2,0)
+
+def emboss(image, angle=ANGLE45, height=1, offset=128, gray_mode=False):
     """
-    浮雕
+    八方浮雕
     :param image: 待处理图片
+    :angle 旋转角度
     :return:
     """
-    image = image
-    kernel = np.array([[1,0,0],[0,0,0],[0,0,-1]])
+    image = image.copy()
+    kernel = np.array([[0,0,0], [0,1,0], [0,0,0]])
+    kernel[angle] = -1
     shape = image.shape
     if gray_mode:
         c = 1
@@ -19,8 +32,29 @@ def emboss(image, gray_mode=False):
     else:
         assert shape[2] == 4, "image should be RGBA"
         c = 3
+    image[...,:c] = filter2D(image[...,:c], c, kernel) * height
+    image += offset
+    image = np.clip(image, 0, 255)
+    return image
+
+def gray_emboss(image, angle:float, offset=128, gray_mode=False):
+    image = image.copy()
+    shape = image.shape
+    angle = angle / 180 * math.pi
+    step = math.pi / 4
+    kernel = np.array([
+        [math.cos(angle+step), math.cos(angle+2*step), math.cos(angle+3*step)],
+        [math.cos(angle), 0, math.cos(angle+math.pi)],
+        [math.cos(angle-step), math.cos(angle-2*step), math.cos(angle-3*step)]
+    ])
+    if gray_mode:
+        c = 1
+        image = image[..., None]
+    else:
+        assert shape[2] == 4, "image should be RGBA"
+        c = 3
     image[...,:c] = filter2D(image[...,:c], c, kernel)
-    image += 127
+    image += offset
     image = np.clip(image, 0, 255)
     return image
 
